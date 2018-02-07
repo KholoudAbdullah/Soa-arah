dbdeaac8b3985cb833298ed568d699cbfc9e6a0b
HP <rahofa.962hotmail.com>
2018-02-07 19:51:21 +0300

    register

--- app/src/main/java/com/soa_arah/Registration.java
+++ app/src/main/java/com/soa_arah/Registration.java
@@ -3,6 +3,7 @@ package com.soa_arah;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -32,7 +34,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Calendar;
 
 public class Registration extends AppCompatActivity implements View.OnClickListener{
-private EditText name;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 555;
+    private EditText name;
     private EditText password;
     private EditText phone;
     private EditText wedith;
@@ -50,6 +53,8 @@ private EditText name;
     private FirebaseDatabase database;
     private DatabaseReference mDatabase;
     private FirebaseAuth firebaseAuth;
+    String Phone;
+    String message;
     User user1;
     private static final String TAG = "MainActivity";
 
@@ -235,7 +240,7 @@ name=(EditText)findViewById(R.id.name);
                                 //close this activity
                                 finish();
                                 //opening login activity
-                                //startActivity(new Intent(getApplicationContext(), home_page_guest.class));
+                                startActivity(new Intent(getApplicationContext(), home_page_guest.class));
                             }
                             else {
                                 //display some message here
@@ -246,6 +251,29 @@ name=(EditText)findViewById(R.id.name);
                     });
         }
     }
+    protected void sendSMSMessage() {
+        final String Phone= phone.getText().toString().trim();
+        String message = "hello";
 
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(Phone, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }}
     }
 
