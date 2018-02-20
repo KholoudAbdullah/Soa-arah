5c435de9ebaf12ae343ace692159b0f2611d7af3
abeer-al <abeer9471@gmail.com>
2018-02-20 11:20:50 +0300

    Login page done

--- app/src/main/java/com/soa_arah/home_page_register.java
+++ app/src/main/java/com/soa_arah/home_page_register.java
@@ -6,16 +6,22 @@ import android.view.Menu;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by Lama on 31/01/18.
  */
 
 public class home_page_register extends AppCompatActivity{
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_register_activity);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
 
@@ -31,6 +37,12 @@ public class home_page_register extends AppCompatActivity{
             startActivity(new Intent(getApplicationContext(), view_account_register.class));
         } else if (item.getItemId() == R.id.editaccount) {
             startActivity(new Intent(getApplicationContext(), edit_account_register.class));
+        } else if (item.getItemId() == R.id.Logout){
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginPage.class));
+
         } else {
             return super.onOptionsItemSelected(item);
         }
