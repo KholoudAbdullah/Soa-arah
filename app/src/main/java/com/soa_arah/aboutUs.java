7eedce75ac6730eff0b28fcab85b42277833b26d
LamaAlma <32649494+LamaAlma@users.noreply.github.com>
2018-04-01 07:30:08 +0300

    change the design of admin

--- app/src/main/java/com/soa_arah/aboutUs.java
+++ app/src/main/java/com/soa_arah/aboutUs.java
@@ -1,7 +1,12 @@
 package com.soa_arah;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
 
 /**
  * Created by Lama on 31/03/18.
@@ -9,9 +14,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class aboutUs extends AppCompatActivity {
 
+    private TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_us);}
+        setContentView(R.layout.about_us);
+
+//        text=(TextView)findViewById(R.id.textView22);
+//
+//
+//        Typeface t = Typeface.createFromAsset(getAssets(), "fonts/Siddiqua.ttf");
+//
+//        text.setTypeface(t);
+    }
+
+
+
+
 
 }
+
