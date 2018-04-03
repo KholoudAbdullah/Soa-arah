aaa869fc84ddf926702f94563080813d49af55bc
LamaAlma <32649494+LamaAlma@users.noreply.github.com>
2018-04-03 08:28:44 +0300

    change the design of IT admin

--- app/src/main/java/com/soa_arah/aboutUs.java
+++ app/src/main/java/com/soa_arah/aboutUs.java
@@ -3,6 +3,8 @@ package com.soa_arah;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,12 +23,15 @@ public class aboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_us);
 
-//        text=(TextView)findViewById(R.id.textView22);
+      text=(TextView)findViewById(R.id.email);
 //
 //
 //        Typeface t = Typeface.createFromAsset(getAssets(), "fonts/Siddiqua.ttf");
-//
+//feedback.setText(Html.fromHtml("<a href=\"mailto:ask@me.it\">Send Feedback</a>"));
 //        text.setTypeface(t);
+
+        text.setText(Html.fromHtml("<a href=\"mailto:soaarah.swe@gmail.com\"> soaarah.swe@gmail.com</a> "));
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 
